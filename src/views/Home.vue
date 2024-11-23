f5178402a7d5567a847374040c4b1a6554ad4c4c
SIPOS Filip SK79A <filip.sipos@student.leaf.academy>
2024-11-23 13:28:31 +0100

    test backend button

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,13 @@
 <template>
   <h1 class="text-4xl font-semibold">Penetration Tester</h1>
+  <AppButton @click="testBeCall">Test Backend</AppButton>
+  <br />
   <AppButton @click="handleLogOut">Log out</AppButton>
 </template>
 
 <script>
 import AppButton from '@/components/AppButton.vue'
+import API from '@/api/axiosInstance.js'
 
 export default {
   components: {
@@ -14,6 +17,13 @@ export default {
   methods: {
     handleLogOut() {
       console.log('logging out')
+    },
+
+    async testBeCall() {
+      console.log('BackEnd TEST CALL')
+      let testResponse = await API.get('/test')
+      console.log('TEST RESULT')
+      console.log(testResponse.data())
     }
   }
 }
